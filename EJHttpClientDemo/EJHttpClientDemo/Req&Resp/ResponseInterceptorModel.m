7f2967cf09806f7a52f94c7fb074eef7179d9745
iOnRoad <xuwenchao_15@163.com>
2016-06-28 14:17:45 +0800

    update demo

--- EJHttpClientDemo/EJHttpClientDemo/Req&Resp/ResponseInterceptorModel.m
+++ EJHttpClientDemo/EJHttpClientDemo/Req&Resp/ResponseInterceptorModel.m
@@ -7,10 +7,25 @@
 //
 
 #import "ResponseInterceptorModel.h"
+#import "CommonResponseModel.h"
+#import "LoginResponseModel.h"
 
 @implementation ResponseInterceptorModel
 
 - (BOOL)ej_interceptorResponseObjectWithBizObject:(id)bizObject commonObject:(id)cmnObject{
+    CommonResponseModel *cmnModel = (CommonResponseModel *)cmnObject;
+    NSLog(@"cmnModel errorCode: %ld",cmnModel.errorCode);
+    NSLog(@"cmnModel errorMsg: %@",cmnModel.errorMsg);
+
+    if([bizObject isKindOfClass:[LoginResponseModel class]]){
+        LoginResponseModel *model = (LoginResponseModel *)bizObject;
+        if([@"admin" isEqualToString:model.username]){
+            NSLog(@"admin request is intercepted. ");
+            return YES;
+        }else{
+            NSLog(@"this request is not intercepted.");
+        }
+    }
     return NO;
 }
 
